46a2fefe710df061f24a613e9a92bebd5d7abcc6
haoweilou <591193117@qq.com>
2020-06-07 04:25:36 +1000

    ass 1 task2

--- ass1/assign1.py
+++ ass1/assign1.py
@@ -13,20 +13,18 @@ I = Image.shape
 
 height = I[0]
 width = I[1]
-def find_region(input,n):
+def find_max(input):
     row = input[0]
     col = input[1]
-    start = [row-n//2,col-n//2]
+    start = [row-N//2,col-N//2]
     if start[0] < 0:
         start[0] = 0
     if start[1] < 0:
         start[1] = 0
-    if start[0] + n > height:
-        start[0] = height - n
-    if start[1] + n > width :
-        start[1] = width - n
-    return start
-def find_max(image,start):
+    if start[0] + N > height:
+        start[0] = height - N
+    if start[1] + N > width :
+        start[1] = width - N
     i = 0
     output = 0
     row = start[0]
@@ -34,23 +32,15 @@ def find_max(image,start):
     while i < N:
         j = 0
         while j < N:
-            output = max(image[row+i][col+j],output)
+            output = max(Image[row+i][col+j],output)
             j += 1
         i += 1
-    print(output)
     return output
-
-
-def find_max_in_neighbour():
-    output = 0
-    return output
+    
 if __name__ == '__main__':
     A = np.zeros(I,dtype=np.uint8)
     for i in range(height):
         for j in range(width):
-            if(find_region((i,j),N) != [i,j]):
-                start = find_region((i,j),N)
-                A[i][j] = find_max(Image,start)
+            A[i][j] = find_max((i,j))
     cv2.imwrite("A.jpg",A)
-    print(height,width)
     
