95536aff89a2cc37e6627cb984219487cc64b743
haoweilou <591193117@qq.com>
2020-06-06 23:42:14 +1000

    part 1 finshied

--- ass1/assign1.py
+++ ass1/assign1.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 print('Enter the path of image: ')
-Image_path = str(input())
+Image_path = 'Particles.png'
 Image = cv2.imread(Image_path,-1)
 I = Image.shape
 height = I[0]
@@ -13,21 +13,44 @@ I = Image.shape
 
 height = I[0]
 width = I[1]
-def find_area(input,n):
+def find_region(input,n):
     row = input[0]
     col = input[1]
-    top_left = (max(0,row-n),max(0,col-n))
-    down_right = (min(height,row+n),min(width,col+n))
-    return top_left,down_right
+    start = [row-n//2,col-n//2]
+    if start[0] < 0:
+        start[0] = 0
+    if start[1] < 0:
+        start[1] = 0
+    if start[0] + n > height:
+        start[0] = height - n
+    if start[1] + n > width :
+        start[1] = width - n
+    return start
+def find_max(image,start):
+    i = 0
+    output = 0
+    row = start[0]
+    col = start[1]
+    while i < N:
+        j = 0
+        while j < N:
+            output = max(image[row+i][col+j],output)
+            j += 1
+        i += 1
+    print(output)
+    return output
+
 
 def find_max_in_neighbour():
     output = 0
     return output
 if __name__ == '__main__':
     A = np.zeros(I,dtype=np.uint8)
-    N = N-1
     for i in range(height):
         for j in range(width):
-            A[i][j] = i+j
-    c
-
+            if(find_region((i,j),N) != [i,j]):
+                start = find_region((i,j),N)
+                A[i][j] = find_max(Image,start)
+    cv2.imwrite("A.jpg",A)
+    print(height,width)
+    
